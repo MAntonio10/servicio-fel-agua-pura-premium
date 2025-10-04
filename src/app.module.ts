d010d8008a717432642539f745b065c0278aa93a
unknown <Castellanos10@>
2025-10-04 02:06:26 -0600

    ajustes del servicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FacturacionModule } from './modules/facturacion/facturacion.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FacturacionModule],
+  imports: [FacturacionModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
